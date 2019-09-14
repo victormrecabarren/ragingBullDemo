e3ad7826ed8a330c6fff690757bbef5f8c83be4a
Victor Recabarren <victormrecabarren@gmail.com>
2019-09-14 18:26:59 -0400

    Updates

---
+++ precache-manifest.617689beb4f2abd79f2472619358fc85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b78cee6fc095feff71ddd60d70b23e",
+    "url": "/ragingBullDemo/index.html"
+  },
+  {
+    "revision": "574c2a9f773f1b5851b3",
+    "url": "/ragingBullDemo/static/css/main.bcee6c33.chunk.css"
+  },
+  {
+    "revision": "03f266c5acf5e02d383e",
+    "url": "/ragingBullDemo/static/js/2.ad17f69e.chunk.js"
+  },
+  {
+    "revision": "574c2a9f773f1b5851b3",
+    "url": "/ragingBullDemo/static/js/main.77ec7b97.chunk.js"
+  },
+  {
+    "revision": "dc597454b99899060e5e",
+    "url": "/ragingBullDemo/static/js/runtime~main.e7153dbd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/ragingBullDemo/precache-manifest.09dbe20d3fb5b73abb57a275df828378.js"
+  "/ragingBullDemo/precache-manifest.617689beb4f2abd79f2472619358fc85.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/ragingBullDemo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ragingBullDemo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
